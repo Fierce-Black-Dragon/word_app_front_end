60373157854871fd7af20330981ca2c3ede2163c
Vishnu Kulkarni <blackdragon4204@gmail.com>
2021-11-05 17:41:46 +0530

    front end basic file structing and creating needed files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 function App() {
-  return <div className="App">it works</div>;
+  return (
+    <div className="App">
+      <AppBar position="relative" stye={{ backgroundColor: "purple" }}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Word App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default App;
